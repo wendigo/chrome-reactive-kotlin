2865d0a5d0d49dfc912d4da5b8995aaddbb8ae26
Mateusz Serafin Gajewski <contact@serafin.tech>
2017-03-11 11:03:21 +0100

    Deploy

--- deploy/nexus.sh
+++ deploy/nexus.sh
@@ -1,2 +1,4 @@
 echo "Uploading archives"
+./gradlew uploadArchives -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword -Psigning.keyId=$signingKeyId -Psigning.password=$signingPassword -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/deploy/private.gpg
 echo "Promoting repository"
+./gradlew closeAndPromoteRepository -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword
