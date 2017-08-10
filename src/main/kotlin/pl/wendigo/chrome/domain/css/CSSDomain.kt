143a1effdc26d53753ae63c7c9458c478eb94808
Mateusz Gajewski <contact@serafin.tech>
2017-08-10 17:38:22 +0200

    Update to newest protocol

--- src/main/kotlin/pl/wendigo/chrome/domain/css/CSSDomain.kt
+++ src/main/kotlin/pl/wendigo/chrome/domain/css/CSSDomain.kt
@@ -781,7 +781,22 @@ data class GetBackgroundColorsResponse(
   /**
    * The range of background colors behind this element, if it contains any visible text. If no visible text is present, this will be undefined. In the case of a flat background color, this will consist of simply that color. In the case of a gradient, this will consist of each of the color stops. For anything more complicated, this will be an empty array. Images will be ignored (as if the image had failed to load).
    */
-  val backgroundColors : List<String>? = null
+  val backgroundColors : List<String>? = null,
+
+  /**
+   * The computed font size for this node, as a CSS computed value string (e.g. '12px').
+   */
+  val computedFontSize : String? = null,
+
+  /**
+   * The computed font weight for this node, as a CSS computed value string (e.g. 'normal' or '100').
+   */
+  val computedFontWeight : String? = null,
+
+  /**
+   * The computed font size for the document body, as a computed CSS value string (e.g. '16px').
+   */
+  val computedBodyFontSize : String? = null
 
 )
 
