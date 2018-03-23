34c240dc2fa24f7d7616a881f4d938d9c361c6a5
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2018-03-23 21:06:46 +0100

    Change configuration

--- deploy/nexus.sh
+++ deploy/nexus.sh
@@ -1,4 +1,4 @@
 echo "Uploading archives"
-./gradlew -i uploadArchives -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword -Psigning.keyId=$signingKeyId -Psigning.password=$signingPassword -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/deploy/private.gpg
+./gradlew uploadArchives -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword -Psigning.keyId=$signingKeyId -Psigning.password=$signingPassword -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/deploy/private.gpg
 echo "Promoting repository"
-./gradlew -i closeAndPromoteRepository -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword
+./gradlew closeAndPromoteRepository -PnexusUsername=$sonatypeUser -PnexusPassword=$sonatypePassword
