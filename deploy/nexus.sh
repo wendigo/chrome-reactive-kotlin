fb0e39f1d33e4a1047892cbe18e71907a695fff5
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2018-03-27 15:41:41 +0200

    Signing configuration

--- deploy/nexus.sh
+++
@@ -1,4 +0,0 @@
-echo "nexusUsername=$sonatypeUser\nnexusPassword=$sonatypePassword\nsigning.keyId=$signingKeyId\nsigning.password=$signingPassword\nsigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/deploy/private.gpg" > gradle.properties
-
-./gradlew uploadArchives -i
-./gradlew closeAndPromoteRepository
