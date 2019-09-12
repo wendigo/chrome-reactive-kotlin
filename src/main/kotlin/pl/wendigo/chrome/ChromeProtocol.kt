4f0faba2772e03ed0e8c1804c42a3a41d8434491
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2019-09-12 22:39:32 +0200

    Update to newest protocol

--- src/main/kotlin/pl/wendigo/chrome/ChromeProtocol.kt
+++ src/main/kotlin/pl/wendigo/chrome/ChromeProtocol.kt
@@ -509,37 +509,4 @@ API.
     override fun close() {
         return api.close()
     }
-
-    companion object {
-
-        /**
-         * Opens new debugging session via chrome debugging protocol for given InspectablePage.
-         */
-        @JvmStatic @JvmOverloads
-        fun openSession(page: InspectablePage, eventBufferSize: Int = 128): ChromeProtocol {
-            return ChromeProtocol(ChromeDebuggerConnection.openSession(page.webSocketDebuggerUrl!!, eventBufferSize))
-        }
-
-        /**
-         * Opens new headless debugging session via chrome debugging protocol for given InspectablePage.
-         *
-         * When opening headless session new browser context is created and communication goes through Target domain
-         * with target id/session id.
-         */
-        @JvmStatic @JvmOverloads
-        fun openHeadlessSession(page: InspectablePage, eventBufferSize: Int = 128, width: Int = 1024, height: Int = 768): HeadlessChromeProtocol {
-            return HeadlessChromeProtocol.create(openSession(page, eventBufferSize), page.url, width, height)
-        }
-
-        /**
-         * Opens new headless debugging session via chrome debugging protocol.
-         *
-         * When opening headless session new browser context is created and communication goes through Target domain
-         * with target id/session id.
-         */
-        @JvmStatic @JvmOverloads
-        fun openHeadlessSession(browserWs: String, url: String, eventBufferSize: Int = 128, width: Int = 1024, height: Int = 768): HeadlessChromeProtocol {
-            return HeadlessChromeProtocol.create(ChromeProtocol(ChromeDebuggerConnection.openSession(browserWs, eventBufferSize)), url, width, height)
-        }
-    }
 }
