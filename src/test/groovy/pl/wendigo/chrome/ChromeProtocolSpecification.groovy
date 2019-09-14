7fce3476c5f5ee4826f17c2dfe852a76800d9487
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2019-09-14 12:00:24 +0200

    Switch to testcontainers

--- src/test/groovy/pl/wendigo/chrome/ChromeProtocolSpecification.groovy
+++ src/test/groovy/pl/wendigo/chrome/ChromeProtocolSpecification.groovy
@@ -1,11 +1,35 @@
 package pl.wendigo.chrome
 
+import org.testcontainers.containers.Container
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.spock.Testcontainers
+import spock.lang.Shared
 import spock.lang.Specification
 
+@Testcontainers
 class ChromeProtocolSpecification extends Specification {
+
+    @Shared
+    public GenericContainer container = new GenericContainer("zenika/alpine-chrome")
+            .withExposedPorts(9222)
+    .withLogConsumer()
+            .withCommand(
+                    "chromium-browser",
+                    "--headless",
+                    "--disable-gpu",
+                    "--disable-software-rasterizer",
+                    "--disable-dev-shm-usage",
+                    "--no-sandbox",
+                    "--remote-debugging-port=9222",
+                    "--remote-debugging-address=0.0.0.0",
+                    "about:blank"
+            )
+            .withPrivilegedMode(true)
+
     def "should open headless session"() {
         given:
-            def chrome = Browser.connect("127.0.0.1:9222")
+            def address = container.getContainerIpAddress() + ":" + container.firstMappedPort
+            def chrome = Browser.connect(address)
 
         when:
             def session = chrome.headlessSession("about:blank", 128)
