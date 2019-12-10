048d4ff87753ba819ea302f7104b105891005176
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2019-12-10 00:07:16 +0000

    Automatically updated to newest protocol

--- src/main/kotlin/pl/wendigo/chrome/api/runtime/Types.kt
+++ src/main/kotlin/pl/wendigo/chrome/api/runtime/Types.kt
@@ -284,7 +284,19 @@ data class PrivatePropertyDescriptor(
     /**  
      * The value associated with the private property.  
      */  
-    val value: RemoteObject
+    val value: RemoteObject? = null,
+
+    /**  
+     * A function which serves as a getter for the private property,  
+     or `undefined` if there is no getter (accessor descriptors only).  
+     */  
+    val get: RemoteObject? = null,
+
+    /**  
+     * A function which serves as a setter for the private property,  
+     or `undefined` if there is no setter (accessor descriptors only).  
+     */  
+    val set: RemoteObject? = null
 )
 
 /**
