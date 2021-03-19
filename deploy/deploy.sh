448ec5a75af381dc906380f16c6847c54d2cafe4
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2021-03-19 22:41:46 +0100

    Try to fix deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,5 @@
-./gradlew publishToSonatype -i
-./gradlew closeAndReleaseRepository -i
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+./gradlew publishToSonatype -i -P githubToken=$GITHUB_ACCESS_TOKEN
+./gradlew closeAndReleaseRepository -i -P githubToken=$GITHUB_ACCESS_TOKEN
